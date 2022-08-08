3f70ec0ea71bba0b2241836bf8f4c35da5016945
kaylendog <actuallyori@gmail.com>
2022-08-08 16:17:38 +0100

    fix: attempt to fix lockfile

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,5 @@
-import axios from "axios";
-
-import { DocumentUpdatePayload } from "@dedit/models/dist/v1/document";
-import { Document } from "@dedit/models/src/v1";
+import { Api } from "@dedit/api";
 
 import { API_ENDPOINT } from "./env";
 
-export const API = axios.create({ baseURL: API_ENDPOINT });
-
-/**
- * Create a new document.
- * @returns
- */
-export const createDocument = () => API.post<Document>("/documents").then(({ data }) => data);
-
-/**
- * Save a document to the server.
- */
-export const updateDocument = (id: string, payload: DocumentUpdatePayload) =>
-	API.patch<string>(`/v1/documents/${id}`, payload);
-
-export const getDocument = (id: string) => API.get<Document>(`/documents/${id}`);
+export const API = new Api({ baseUrl: API_ENDPOINT });
